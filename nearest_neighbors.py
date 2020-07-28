3a35af219757153149a72207b83e4271ff6a5150
Travis Cain <Travis@Traviss-MacBook-Air.local>
2020-07-27 17:23:15 -0700

    Brainstorming...

---
+++ nearest_neighbors.py
@@ -0,0 +1,7 @@
+### Code here ###
+
+# Class
+
+class nearest_neighbors:
+    def __init__(self, n_nieghbors=5):
+        self.n_nieghbors = n_nieghbors
---
+++ testing_NN.py
@@ -0,0 +1,7 @@
+# Import model
+
+# from nearest_neighbors import k_nearest_neighbors
+
+# want to use pybaseball data
+
+# sk.learn datasets are available
